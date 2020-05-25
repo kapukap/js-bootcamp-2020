f1df241c33b7333c56d213d1879bc1f822706673
Kapustin Kirill <kirill.kapustin29@gmail.com>
2020-05-25 15:11:31 +0300

    Change names of variables

--- submissions/kirill/07-html_JS/main.js
+++ submissions/kirill/07-html_JS/main.js
@@ -1,19 +1,13 @@
-let cont_1 = document.querySelector('.content_1');
-let cont_2 = document.querySelector('.content_2');
-let cont_3 = document.querySelector('.content_3');
-let task_1_btn = document.querySelector('#btn-1');
-let task_2_btn = document.querySelector('#btn-2');
-let btn3 = document.querySelector('#btn-3');
+// Task 1
+let signInContainer = document.querySelector('#signInContainer');
+let singInBtn = document.querySelector('#singInBtn');
 // Task 2
-
+let countersContainer = document.querySelector('#countersContainer');
+let addCounterBtn = document.querySelector('#addCounterBtn');
 // Task 3
-let number = document.querySelector('#ipt-fibo');
-
-function createElem(txt, container) {
-    let div = document.createElement('div');
-    div.textContent = txt;
-    container.appendChild(div);
-}
+let fibonacciContainer = document.querySelector('#fibonacciContainer');
+let number = document.querySelector('#fibonacciInput');
+let fibonacciBtn = document.querySelector('#fibonacciBtn');
 
 function logIn() {
     let rtn;
@@ -38,7 +32,12 @@ function logIn() {
     } else {
         rtn = 'I don\'t know you';
     }
-    createElem(rtn, cont_1);
+
+
+    let div = document.createElement('div');
+    div.textContent = rtn;
+    signInContainer.appendChild(div);
+
     return rtn;
 }
 
@@ -52,21 +51,24 @@ function addCounter() {
 
     div.appendChild(span);
     div.appendChild(button);
-    cont_2.appendChild(div);
+    countersContainer.appendChild(div);
 
     button.addEventListener('click', function (){
         span.textContent = +span.textContent+1;
     });
 }
 
-function fibonachi(n) {
-    return n <= 1 ? n : fibonachi(n - 1) + fibonachi(n - 2);
+function fibonacchi(n) {
+    return n <= 1 ? n : fibonacchi(n - 1) + fibonacchi(n - 2);
 }
 
-task_1_btn.addEventListener('click', logIn);
-task_2_btn.addEventListener('click', addCounter);
-task_2_btn_hidden.addEventListener('click', counting);
-btn3.addEventListener('click', function () {
-    let numb = fibonachi(number.value);
-    createElem(numb, cont_3);
+singInBtn.addEventListener('click', logIn);
+addCounterBtn.addEventListener('click', addCounter);
+
+fibonacciBtn.addEventListener('click', function () {
+    let numb = fibonacchi(number.value);
+    let div = document.createElement('div');
+
+    div.textContent = numb;
+    fibonacciContainer.appendChild(div);
 });
